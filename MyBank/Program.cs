153d9b292cdef45e2fd11d0a576a45395c7d3f1b
pc <pc@DESKTOP-7E42OKS>
2023-05-22 14:20:06 +0500

    Add project files.

---
+++ MyBank/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MyBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAcount("javeria", 40000);
+            Console.WriteLine($"The new account {account.Number} is created for {account.Owner} with initial balance of {account.Balance} ");
+            account.MakeWithdrawal(5000, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(1000, DateTime.Now, "Friend paid me back");
+            Console.WriteLine(account.Balance);
+            Console.WriteLine(account.GetAccountHistory());
+            
+            
+            
+            // Test that the initial balances must be positive.
+
+            //try
+            //{
+            //    var bankAccount = new BankAcount("invalid", -55);
+            //}
+            //catch (ArgumentOutOfRangeException e)
+            //{
+            //    Console.WriteLine("Exception caught creating account with negative balance");
+            //    Console.WriteLine(e.ToString());
+            //    return;
+            //}
+            //// Test for a negative balance.
+            //try
+            //{
+            //    account.MakeWithdrawal(75000, DateTime.Now, "Attempt to overdraw");
+            //}
+            //catch (InvalidOperationException e)
+            //{
+            //    Console.WriteLine("Exception caught trying to overdraw");
+            //    Console.WriteLine(e.ToString());
+            //}
+        }
+    }
+}
